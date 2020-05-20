5a8e1f6cf9ae9adba274e186fc21e1ad357ef9d5
Briseid Pagador <bpagador@gmail.com>
2020-05-19 17:23:28 -0700

    closing to solve error

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import request from 'superagent'
 
 export default class ListPage extends Component {
+
+    componentDidMount = async () => {
+        const currentToken = localStorage.getItem('TOKEN')
+        this.setState({ token: currentToken})
+    }
+
+    handleClick = async () => {
+        //find a way to hide our key
+    }
+    
     render() {
         return (
             <div>
-                
+                <button className='happy-button' onClick={this.handleClick}>Happy</button>
             </div>
         )
     }
