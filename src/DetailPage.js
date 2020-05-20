0c36b902ba5bf24ecec536fc53a5977c3ab6f280
Briseid Pagador <bpagador@gmail.com>
2020-05-19 19:25:05 -0700

    added notes at the bottom of listpages to track progress

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-// import nasaData from './Data.js';
+
 
 export default class DetailPage extends Component {
+
+
     render() {
         return (
             <div>
+                <section>
+                    <h3>
+                        {this.props.detail.description}
+                    </h3>
+                    
+                </section>
                 
             </div>
         )
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import request from 'superagent'
+import { Link } from 'react-router-dom'
+import DetailPage from './DetailPage.js'
 
 export default class ListPage extends Component {
+    state = {
+        data: [],
+
+    }
 
     componentDidMount = async () => {
         const currentToken = localStorage.getItem('TOKEN')
@@ -10,13 +16,33 @@ export default class ListPage extends Component {
 
     handleClick = async () => {
         //find a way to hide our key
+        const fetchData = await request.get('https://api.unsplash.com/search/photos?query=happy&page=1&client_id=CUNUKhGnQYLX8XHwEllBIUU35CEBJkEoPA4YQD1aYXQ').set('Authorization', this.state.token)
+
+        this.setState({ data: fetchData.body.results })
+        
     }
     
     render() {
         return (
             <div>
                 <button className='happy-button' onClick={this.handleClick}>Happy</button>
+                {
+                    this.state.data.map(item => {
+                        return <li> <Link to='/detail' > <img src = {item.urls.regular} alt={item.alt_description}  /> 
+                        
+                        <DetailPage detail={item}/>
+
+                        </Link>
+
+                            
+                        </li>
+                    })
+                }
             </div>
         )
     }
 }
+
+// list page is working when "happy" button is clicked, detail page shows "cannot read property description of undefined"
+// we will pick back up at the detail page
+// afterward we need to hook up our backend to our API
