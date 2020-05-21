a47dee500e39eb976df337e0a107999878eca377
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-21 15:19:30 -0700

    updated what info we have for about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,7 +4,41 @@ export default class AboutUs extends Component {
     render() {
         return (
             <div>
-                About Us Page
+                <h1>About Us Page</h1>
+
+                <div className="one-person">
+                    <img src="" alt="" />
+                    <h3>Name: Briseida </h3>
+                    <p>Briseida is a developer that heavily relies on her mental health to carry on in this wild, wild world. She has been respecting quarantine for about 70 days now and deeply misses hugging, sharing meals, and listening to her friends laughing really hard.</p>
+                    <p>Github: </p>
+                    <p>Linkedin: </p>
+                </div>
+
+                <div className="one-person">
+                    <img src="" alt="" />
+                    <h3>Breeann</h3>
+                    <p>Description: Hey there! My name is Breeann and I was part of the team that created Fetch ART. I was really interested in this project as it combines development, art and design and loved to develop a new way to connect with lesser known creatives and their images. </p>
+                    <p>Github: https://github.com/breeannb</p>
+                    <p>Linkedin: https://www.linkedin.com/in/breeannbolinsky/</p>
+                </div>
+
+                <div className="one-person">
+                    <img src="" alt="" />
+                    <h3>Jaime</h3>
+                    <p>Description</p>
+                    <p>Github: </p>
+                    <p>Linkedin: </p>
+                </div>
+
+                <div className="one-person">
+                    <img src="" alt="" />
+                    <h3>Max</h3>
+                    <p>My name is Max and I felt like this project would be a good way for me to create something that helps people get out of the darkness that can take over their mental space at certain difficult times.</p>
+                    <p>Github: </p>
+                    <p>Linkedin: </p>
+                </div>
+
+
             </div>
         )
     }
