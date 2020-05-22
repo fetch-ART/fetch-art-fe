772468ea3838f839682048b921636228ec89c05b
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-21 23:23:59 -0700

    cleans up code in favorites page

--- src/FavoritesPage.js
+++ src/FavoritesPage.js
@@ -8,9 +8,7 @@ export default class FavoritesPage extends Component {
 
     state = {
         data: [],
-        newData: ''
     }
-
 
     loadFavorites = async () => {
         const fetchedData = await request.get(`http://nameless-hollows-93608.herokuapp.com/api/favorites`).set('Authorization', this.props.token)
@@ -24,24 +22,18 @@ export default class FavoritesPage extends Component {
     }
 
     componentDidMount = async () => {
-
         await this.loadFavorites();
-
     }
 
     handleClick = async (item) => {
-
-
         console.log(item)
+
         await request.delete(`http://nameless-hollows-93608.herokuapp.com/api/favorites/${item.id}`).set('Authorization', this.props.token)
-
-
 
        await this.loadFavorites();
     }
 
     render() {
-
         return (
             <>
             <div>
@@ -49,15 +41,13 @@ export default class FavoritesPage extends Component {
                 {
                     this.state.data.map(item => {
                         return  <div className='favorites-list'>
-                            <ListItem detail={item}/>
-                            <button onClick={ () => this.handleClick(item)}>Remove</button>
+                            <ListItem detail={ item }/>
+                            <button onClick={ () => this.handleClick(item) }>Remove</button>
                         </div>
                     })
                 }
-
             </div>
-
             </>
         )
-    }
+        }
 }
