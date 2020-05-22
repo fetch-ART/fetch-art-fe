1d1b587e0c0e9a95b398af7889ffcc785b695e32
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-21 18:18:57 -0700

    adding the readme details

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -8,7 +8,7 @@ export default class AboutUs extends Component {
 
                 <div className="one-person">
                     <img src="" alt="" />
-                    <h3>Name: Briseida </h3>
+                    <h3>Briseida </h3>
                     <p>Briseida is a developer that heavily relies on her mental health to carry on in this wild, wild world. She has been respecting quarantine for about 70 days now and deeply misses hugging, sharing meals, and listening to her friends laughing really hard.</p>
                     <p>Github: </p>
                     <p>Linkedin: </p>
@@ -25,9 +25,9 @@ export default class AboutUs extends Component {
                 <div className="one-person">
                     <img src="" alt="" />
                     <h3>Jaime</h3>
-                    <p>Description</p>
-                    <p>Github: </p>
-                    <p>Linkedin: </p>
+                    <p>Jaime is a software engineer with a degree in photography. She enjoys looking at beautiful images to boost her mood and inspire creativity. Her favorite types of photography are travel and landscape, and she looks forward to doing both once it is safe to do so.</p>
+                    <p>Github: https://github.com/jaimekinsley</p>
+                    <p>Linkedin: https://www.linkedin.com/in/jaimelyn/</p>
                 </div>
 
                 <div className="one-person">
@@ -35,7 +35,7 @@ export default class AboutUs extends Component {
                     <h3>Max</h3>
                     <p>My name is Max and I felt like this project would be a good way for me to create something that helps people get out of the darkness that can take over their mental space at certain difficult times.</p>
                     <p>Github: </p>
-                    <p>Linkedin: </p>
+                    <p>Linkedin: https://www.linkedin.com/in/max-lamb-541b64147/</p>
                 </div>
 
 
